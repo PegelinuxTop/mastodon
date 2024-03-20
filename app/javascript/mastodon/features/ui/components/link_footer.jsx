84cfc9adb666aaa0566689fa562f7f974e65faca
Rezha Julio <contact@rezhajulio.id>
2024-03-20 12:02:49 +0700

    Remove elk

--- app/javascript/mastodon/features/ui/components/link_footer.jsx
+++ app/javascript/mastodon/features/ui/components/link_footer.jsx
@@ -64,10 +64,7 @@ class LinkFooter extends PureComponent {
     return (
       <div className='link-footer'>
         <p>
-          New to Mastodon and want an easier transition? Try the <a href='https://elk.pegelinux.top/'>Alternative UI (Elk)</a> for a more &quot;bird-like&quot; interface!
-        </p>
-        <p>
-          You can also try <a href='https://elk.pegelinux.top/'>Alternative UI (Phanpy)</a> for a minimalistic client interface.
+          You can also try <a href='https://phanpy.pegelinux.top/'>Alternative UI (Phanpy)</a> for a minimalistic client interface.
         </p>
         <p>
           <strong>{domain}</strong>:
