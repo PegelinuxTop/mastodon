92445630b58115cff7f6945382f90b24c7a6e4c3
Rezha Julio <rezha.pradana@gojek.com>
2024-09-28 06:09:12 +0700

    Merge upstream

--- app/javascript/flavours/glitch/features/ui/components/link_footer.jsx
+++ app/javascript/flavours/glitch/features/ui/components/link_footer.jsx
@@ -48,7 +48,7 @@ class LinkFooter extends PureComponent {
     return (
       <div className='link-footer'>
         <p>
-          New to Mastodon and want an easier transition? Try the <a href='https://elk.urusai.social/'>Alternative UI (Elk)</a> for a more &quot;bird-like&quot; interface!
+          New to Mastodon and want an easier transition? Try the <a href='https://phanpy.pegelinux.top/'>Alternative UI (Phanpy)</a> for a more &quot;bird-like&quot; interface!
         </p>
         <p>
           <strong>{domain}</strong>:
